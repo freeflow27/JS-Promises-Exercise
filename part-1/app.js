4b12d8248108050101435b7491f5ddb6341a6fe5
Alvaro Davison <adavison813@gmail.com>
2022-07-27 16:43:51 -0400

    JS promises exercise

---
+++ part-1/app.js
@@ -0,0 +1,22 @@
+let favNumber = 15;
+let baseURL = "http://numbersapi.com";
+
+// 1.
+$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+  console.log(data);
+});
+
+// 2.
+let favNumbers = [15, 77, 28];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+  console.log(data);
+});
+
+// 3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
